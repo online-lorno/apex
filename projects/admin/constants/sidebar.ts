c6b5f8e0d23abdf489480343af0101d35fab89b5
Leonard Laput <online.lorno@gmail.com>
2022-05-02 15:14:42 +0800

    feat: sidebar constant

---
+++ projects/admin/constants/sidebar.ts
@@ -0,0 +1,58 @@
+import { MenuItemProps } from '@components/common/Sidebar'
+
+interface Props {
+  [key: string]: MenuItemProps[]
+}
+
+const sidebar: Props = {
+  brokerage: [
+    {
+      title: 'Dashboard',
+      icon: 'DashboardOutlined',
+      link: '/',
+    },
+    {
+      title: 'Sales Leads',
+      icon: 'CleanHandsOutlined',
+    },
+    {
+      title: 'Comissions',
+      icon: 'MonetizationOnOutlined',
+      badgeText: '4',
+      menuItems: [
+        {
+          title: 'Comissions 1',
+        },
+        {
+          title: 'Comissions 2',
+        },
+        {
+          title: 'Comissions 3',
+        },
+      ],
+    },
+    {
+      title: 'Properties',
+      icon: 'HomeWorkOutlined',
+      badgeText: '10',
+      menuItems: [
+        {
+          title: 'Private',
+          link: '/brokerage/private',
+        },
+        {
+          title: 'Projects',
+        },
+      ],
+    },
+    {
+      title: 'Developers',
+      icon: 'HouseSidingOutlined',
+    },
+  ],
+  report: [],
+  settings: [],
+  billing: [],
+}
+
+export default sidebar
